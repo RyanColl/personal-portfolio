28b57b923485adab9ef8fd79cbda7d138d1c0aee
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-14 23:38:40 -0800

    typescript added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import './styles/App.css';
+import { AppProps } from 'next/app';
 // keeps state in app
-const CustomApp = ({ Component, pageProps }) => {
+const CustomApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-const IndexPage = ({ logo }) => {
+const IndexPage = () => {
   
   return(
     <>
