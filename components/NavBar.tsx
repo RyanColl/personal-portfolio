70babe45aa1eb370509a58adb2cc492ffa0cf9f1
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-15 19:39:50 -0800

    removed margin from body

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,40 +2,9 @@ import React from 'react'
 
 const NavBar = () => {
   return(
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        
-        <a className="item">
-          <img src="./logo-full-white.png" width="100" height="100" />
-        </a>
-      </div>
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
-          More
-        </a>
-
-        <div className="navbar-dropdown">
-          <a className="navbar-item dropdown-item">
-            About
-          </a>
-          <a className="navbar-item">
-            Jobs
-          </a>
-          <a className="navbar-item">
-            Contact
-          </a>
-          <hr className="navbar-divider" />
-          <a className="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-      </a>
-    </nav>
+    <div className="navbar flex" >
+      <img src='./moon.svg' />
+    </div>
   )
 }
 export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import NavBar from '../components/NavBar'
+import { useEffect } from 'react';
 const IndexPage = () => {
-  
+  useEffect(() => {
+    document.body.style.margin = '0'
+});
   return(
     <>
-    <Head>
+    {/* its easy to add bulma to our project */}
+    {/* <Head>
       <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css" />
-    </Head>
+    </Head> */}
     <div className="App">
       <header className="App-header">
         <NavBar />
