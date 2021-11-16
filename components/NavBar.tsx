79fa80ff9f4710a6a767a778fe299646b73e55ba
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-15 22:29:34 -0800

    framer in- adding styled

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
 const NavBar = () => {
   return(
     <div className="custom-navbar" >
       <div className="nav-logo">
-        <img style={{width: '112px', height: '112px'}} className="logo" src='./logo-p1.png' />
+        <img style={{width: '99px', height: '99px'}} className="logo" src='./logo-p1.png' />
       </div>
       <div className="nav-right">
         <div className="nav-item">
           <img className="app-lighting" src='./moon.svg' />
         </div>
-        <div className="nav-item">
+        <motion.div className="nav-item">
           <img className="nav-burger" src="./burger.svg" />
-        </div>
+        </motion.div>
       </div>
       
     </div>
