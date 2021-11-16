974cf56cb243cd23920c9b21cf0882c98c2bdb66
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-15 21:04:48 -0800

    about to add framer motion

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const NavBar = () => {
   return(
-    <div className="navbar flex" >
-      <img src='./moon.svg' />
+    <div className="custom-navbar" >
+      <div className="nav-logo">
+        <img style={{width: '112px', height: '112px'}} className="logo" src='./logo-p1.png' />
+      </div>
+      <div className="nav-right">
+        <div className="nav-item">
+          <img className="app-lighting" src='./moon.svg' />
+        </div>
+        <div className="nav-item">
+          <img className="nav-burger" src="./burger.svg" />
+        </div>
+      </div>
+      
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,13 @@ const IndexPage = () => {
     document.body.style.margin = '0'
 });
   return(
-    <>
-    {/* its easy to add bulma to our project */}
-    {/* <Head>
-      <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css" />
-    </Head> */}
+    
     <div className="App">
       <header className="App-header">
         <NavBar />
       </header>
     </div>
-  </>
+  
 )};
 
 
