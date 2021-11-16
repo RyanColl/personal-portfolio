57ee1cfba05cea702e674c3258a67462865a1d29
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-15 16:30:52 -0800

    need to change navbar to figma layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import NavBar from '../components/NavBar'
 const IndexPage = () => {
   
   return(
@@ -7,11 +8,10 @@ const IndexPage = () => {
       <link rel="stylesheet" href="https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css" />
     </Head>
     <div className="App">
-    <header className="App-header">
-      <img src={'./logo-p1.png'} className="App-logo" alt="logo" />
-      <button onClick={() => console.log('pressed')}>Press ME</button>
-    </header>
-  </div>
+      <header className="App-header">
+        <NavBar />
+      </header>
+    </div>
   </>
 )};
 
