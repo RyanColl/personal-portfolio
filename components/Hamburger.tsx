be75119178e6d310a36ecef8628a1c32926faa0f
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-11-24 00:17:08 -0800

    navbar complete and ready for more pages to load

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -18,37 +18,37 @@ const Hamburger = ({lightTheme}: any) => {
         <g strokeWidth="6.5" strokeLinecap="round">
           <path
             d="M72 82.286h28.75"
-            fill={!lightTheme?'white':"black"}
+            fill={lightTheme?'white':"black"}
             fillRule="evenodd"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
           <path
             d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
             fill="none"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
           <path
             d="M72 125.143h28.75"
-            fill={!lightTheme?'white':"black"}
+            fill={lightTheme?'white':"black"}
             fillRule="evenodd"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
           <path
             d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
             fill="none"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
           <path
             d="M100.75 82.286h28.75"
-            fill={!lightTheme?'white':"black"}
+            fill={lightTheme?'white':"black"}
             fillRule="evenodd"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
           <path 
             d="M100.75 125.143h28.75"
-            fill={!lightTheme?'white':"black"}
+            fill={lightTheme?'white':"black"}
             fillRule="evenodd"
-            stroke={lightTheme?'white':"black"}
+            stroke={!lightTheme?'white':"black"}
           />
         </g>
       </motion.svg>
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,7 +6,26 @@ import Link from 'next/link'
 const NavItem = styled(motion.div)`
   margin-right: 10px;
 `;
-const NavBar = ({lightTheme, lightSwitch}: any) => {
+const NavBar = ({lightTheme, lightSwitch, activeClass}: any) => {
+  const [active, setActive] = React.useState(activeClass || {home: true, projects: false, blog: false, about: false})
+  const linkArray = [
+    {
+      link: 'Home',
+      active: active.home
+    },
+    {
+      link: 'Projects',
+      active: active.projects
+    },
+    {
+      link: 'Blog',
+      active: active.blog
+    },
+    {
+      link: 'About',
+      active: active.about
+    },
+]
   return (
     <div className="custom-navbar">
       <NavItem whileHover={{ rotate: 30 }} whileTap={{ scale: 0.95 }}>
@@ -20,22 +39,34 @@ const NavBar = ({lightTheme, lightSwitch}: any) => {
       </NavItem>
       <div className="nav-right">
         <NavItem className="nav-links">
-            {['Home', 'Projects', 'About', 'Blog'].map((link, i) => {
-              return (
-                  <motion.div key={`nav-link${i}`} className='nav-link'>
-                    <Link href={`/${link.toLowerCase()}`}>{link}</Link>
-                  </motion.div>
-                );
-            })}
+          {linkArray.map((linkObj, i) => {
+            return (
+              <motion.div
+                whileHover={{ translateY: -8, scale: 1.07 }}
+                key={`nav-link${i}`}
+                className={`nav-link ${linkObj.active&&'active-link'}`}
+              >
+                <Link href={`/${linkObj.link.toLowerCase()}`}>{linkObj.link}</Link>
+              </motion.div>
+            );
+          })}
         </NavItem>
-        <NavItem  whileTap={{ scale: 0.9 }}>
-          <div style={{cursor: 'pointer'}} onClick={lightSwitch} className="lightswitch" >
+        <motion.div
+          whileHover={{ scale: 1.2, translateY: -8 }}
+          whileTap={{ scale: 0.9 }}
+          className="lightswitch-div"
+        >
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={lightSwitch}
+            className="lightswitch"
+          >
             <img
               className="app-lighting"
               src={lightTheme ? "./sun.svg" : "./moon.svg"}
             />
           </div>
-        </NavItem>
+        </motion.div>
         <motion.div className="hamburger-div" whileHover={{ scale: 1.05 }}>
           <Hamburger lightTheme={lightTheme} />
         </motion.div>
