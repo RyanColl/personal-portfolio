09f12d2d6a74d5a7af8dc886d27e2d122de4ee8e
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-12-02 23:45:12 -0800

    tag dropdown working

--- components/blog/TagDropdown.tsx
+++ components/blog/TagDropdown.tsx
@@ -1,42 +1,68 @@
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import React, { useState } from 'react'
 import { Icon } from '@iconify/react';
 const TagDropdown = ({tags}: {tags: string[]}) => {
     const [isOpen, setOpen] = useState(false)
-    const tagStyles = {
+    const containerStyles = {
         hidden: {
             display: 'none',
             visibility: 'hidden',
             opacity: 0,
-            scale: 0
+            scale: 0,
+            width: 0,
+            transition: {
+                when: "afterChildren"
+            }
         },
         visible: {
             display: 'flex',
             visibility: 'visible',
             opacity: 1,
-            scale: 1
+            scale: 1,
+            width: 200,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.3
+            }
         }
     }
+    const tagStyles = {
+        hidden: {y: -100, opacity: 0},
+        visible: {y: 0, opacity: 1},
+        exit: {translateX: -300, opacity: 0}
+    }
     return (
-        <motion.div 
-        onClick={() => {setOpen(!isOpen)}}
-        animate={isOpen?{scale: 1.2, rotate: '0deg'}:{scale: 1.2, rotate: '180deg', translateY: -6}}
-        transition={{duration: 0.3}} 
-        whileTap={{scale: 1.1}} className="dropdown-div">
-            <Icon icon="eva:arrow-down-fill" color="white" width="32" />
+        <>
+            <motion.div 
+            onClick={() => {setOpen(!isOpen)}}
+            animate={isOpen?{scale: 1.2, rotate: '0deg'}:{scale: 1.2, rotate: '180deg', translateY: -6}}
+            transition={{duration: 0.3}} 
+            whileTap={{scale: 1.1}} className="dropdown-div">
+                <Icon icon="eva:arrow-down-fill" color="white" width="32" />         
+            </motion.div>
+            
             {/* @ts-ignore */}
-            <motion.div animate={isOpen?tagStyles.visible:tagStyles.hidden} className="tags">
+            <motion.div key={'tags'} variants={containerStyles} animate={"visible"} initial="hidden" className="tags">
                 {tags.length>0 && tags.map((tag, i) => {
                     return (
-                        <motion.div 
-                        key={`dropdown-tag-${i}`} className='dropdown-tag'>
-                            <motion.span>{tag}</motion.span>
-                        </motion.div>
+                        <AnimatePresence>
+                        {isOpen &&   
+                            <motion.div
+                            animate="visible"
+                            initial="hidden"
+                            exit="hidden"
+                            variants={tagStyles}
+                            transition={{delay: 0.1*i}}
+                            key={`dropdown-tag-${i}`} className='dropdown-tag'>
+                                <motion.span>{tag}</motion.span>
+                            </motion.div>
+                        }
+                        </AnimatePresence>
                     )
                 })}
             </motion.div>
             
-        </motion.div>
+        </>
     )
 }
 
