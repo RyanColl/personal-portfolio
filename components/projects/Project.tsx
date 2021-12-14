c949687d350dea41a10d4a076a406eb7aeb20438
Ryan Collicutt <ryancollicutt22@gmail.com>
2021-12-14 02:59:37 -0800

    languages added to projects

--- components/projects/Project.tsx
+++ components/projects/Project.tsx
@@ -2,8 +2,22 @@ import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { Icon } from '@iconify/react';
 const Project = ({project, i}: any) => {
+    const [hovering, setHovering] = useState(false)
+    const bottomDivAnims = {
+        initial: {
+            height: 0, opacity: 0, display: 'none', visibility: 'hidden', zIndex: -10
+        },
+        animate: {
+            opacity: hovering?1:0, height: hovering?30:0, zIndex: 1,
+            display: hovering?'flex':'none', visibility: hovering?'visible':'hidden'
+        }
+    }
+    
     return (
-        <motion.div initial={{x: '100vw'}} animate={{x: 0}} transition={{delay: 1.2 + (i*0.4)}} className="project">
+        <motion.div 
+        onHoverStart={() => {setHovering(true)}}
+        onHoverEnd={() => {setHovering(false)}}
+        initial={{x: '100vw'}} animate={{x: 0}} transition={{delay: 1.2 + (i*0.4)}} className="project">
             <motion.div className="project-image">
                 <a href={project.link!==''?project.link:''}><motion.img style={{height: 205}} src={project.image} /></a> 
             </motion.div>
@@ -16,8 +30,20 @@ const Project = ({project, i}: any) => {
                     </motion.span>
                 </a>
             </motion.div>
+            {/* @ts-ignore */}
+            <motion.div initial={bottomDivAnims.initial} animate={bottomDivAnims.animate} className='languages'>
+                    {typeof project.languages == 'object' && project.languages.map((language: string, i: number) => {
+                        console.log(language.replaceAll(' ','-').toLowerCase())
+                        return(
+                            <motion.div initial={{opacity: 0}} animate={{opacity: hovering?1:0}} className='language'>
+                                {<span className="iconify" data-icon={`logos:${language.replace(' ','-').toLowerCase()}`}></span>}
+                            </motion.div>
+                        );
+                    })}
+            </motion.div>
         </motion.div>
     )
 }
 
 export default Project
+
